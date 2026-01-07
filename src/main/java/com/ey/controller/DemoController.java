2342d7d93d642d0f10b57f28552a7e163ecbb5f6
bhavya <tarigopulabhavya@gmail.com>
2026-01-07 14:00:54 +0530

    second commit

--- src/main/java/com/ey/controller/DemoController.java
+++ src/main/java/com/ey/controller/DemoController.java
@@ -1,5 +1,13 @@
 package com.ey.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DemoController {
 
+	@GetMapping
+	public String home() {
+		return "Wecome home";
+	}
 }
